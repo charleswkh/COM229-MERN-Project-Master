edb367980686885f5d42c96c5ff1035a0a720efe
gavinlikewind <gavinlikewind@gmail.com>
2024-03-18 15:22:22 -0400

    appointment remove field "Appointment no"

--- server/controllers/appointment.controller.js
+++ server/controllers/appointment.controller.js
@@ -22,7 +22,7 @@ const list = async (req, res) => {
         if (req.query.apply_user){
             queryAry["apply_user"] = req.query.apply_user;
         }
-        let appointments = await Appointment.find(queryAry).select('appointment_no apply_user appointment_date is_active');
+        let appointments = await Appointment.find(queryAry).select('apply_user appointment_date is_active');
         res.json(appointments);
     } catch (err) {
         return res.status(400).json({
