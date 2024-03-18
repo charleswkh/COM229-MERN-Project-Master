d2d6e23bc4226468befce17cb4666b45f39ff07a
gavinlikewind <gavinlikewind@gmail.com>
2024-03-17 20:35:53 -0400

    add appointment API: api/appointment

---
+++ server/models/appointment.model.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose'
+import crypto from 'crypto'
+//const mongoose = require('mongoose');
+const AppointmentSchema = new mongoose.Schema({
+    appointment_no: {
+        type: String,
+        trim: true
+    },
+    apply_user: {
+        type: String,
+        trim: true,
+        required: 'Apply User is required'
+    },
+    appointment_date: {
+        type: Date,
+        default: Date.now
+    },
+    is_active:{
+        type: Boolean,
+        default : true
+    }
+});
+
+
+//module.exports = mongoose.model('User', UserSchema);
+export default mongoose.model('Appointment', AppointmentSchema);
