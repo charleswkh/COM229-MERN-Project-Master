84cec51c2243bec12355b237afaaf4e6e1236198
charleswkh <39009330+charleswkh@users.noreply.github.com>
2024-04-04 07:17:30 +0800

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,4 +3,4 @@ import App from './App.jsx'
 import { createRoot } from 'react-dom/client';
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App tab="home" />)
+root.render(  <React.StrictMode><App tab="home" /></React.StrictMode>)
