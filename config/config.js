c724434c434ef9b8b970461dcbb6764e69076ecb
charleswkh <39009330+charleswkh@users.noreply.github.com>
2024-04-04 03:36:05 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const config = {
     env: process.env.NODE_ENV || 'development', 
-    port: process.env.PORT || 3000,
+    port: process.env.DB_PORT || 3000,
     jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key", 
     mongoUri: process.env.MONGODB_URI || "mongodb+srv://gavinlikewind:71HLrYHNxU6VOuB9@cluster0.k7pj1q8.mongodb.net/COMM229_Project?retryWrites=true&w=majority&appName=Cluster0"||
     process.env.MONGO_HOST ||
